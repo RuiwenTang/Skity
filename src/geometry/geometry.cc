d1d5d5ec54d32195086b8670c2e898f7f3171dae
RuiwenTang <tangruiwen1989@gmail.com>
2021-04-11 11:12:23 +0800

    Finish QuadCoeff

--- src/geometry/geometry.cc
+++ src/geometry/geometry.cc
@@ -15,4 +15,46 @@ Point QuadCoeff::evalAt(float t) { return Point{eval(t), 0, 1}; }
 
 glm::vec2 QuadCoeff::eval(float t) { return eval(glm::vec2{t, t}); }
 
+glm::vec2 QuadCoeff::eval(glm::vec2 const& tt) { return (A * tt + B) * tt + C; }
+
+Point QuadCoeff::EvalQuadAt(std::array<Point, 3> const& src, float t) {
+  return ToPoint(QuadCoeff{src}.eval(t));
+}
+
+void QuadCoeff::EvalQuadAt(std::array<Point, 3> const& src, float t,
+                           Point* outP, Vector* outTangent) {
+  if (t < 0) {
+    t = 0;
+  }
+  if (t > Float1) {
+    t = Float1;
+  }
+
+  if (outP) {
+    *outP = EvalQuadAt(src, t);
+  }
+
+  if (outTangent) {
+    *outTangent = EvalQuadTangentAt(src, t);
+  }
+}
+
+Vector QuadCoeff::EvalQuadTangentAt(std::array<Point, 3> const& src, float t) {
+  if ((t == 0 && src[0] == src[1]) || (t == 1 && src[1] == src[2])) {
+    return src[2] - src[0];
+  }
+
+  glm::vec2 P0 = FromPoint(src[0]);
+  glm::vec2 P1 = FromPoint(src[1]);
+  glm::vec2 P2 = FromPoint(src[2]);
+
+  glm::vec2 B = P1 - P0;
+  glm::vec2 A = P2 - P1 - B;
+  glm::vec2 T = A * glm::vec2{t, t} + B;
+
+  return Vector{T + T, 0, 0};
+}
+
+
+
 }  // namespace skity
