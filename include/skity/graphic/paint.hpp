bfe60b35e3855ffd0b7c96437054a539ee05b7f6
RuiwenTang <tangruiwen1989@gmail.com>
2021-04-16 16:23:05 +0800

    fix cmake export compile commands not working

---
+++ include/skity/graphic/paint.hpp
@@ -0,0 +1,19 @@
+#ifndef SKITY_GRAPHIC_PAINT_HPP
+#define SKITY_GRAPHIC_PAINT_HPP
+
+namespace skity {
+
+ /**
+  *
+  */
+class Paint {
+public:
+  Paint();
+  ~Paint();
+
+
+};
+
+}  // namespace skity
+
+#endif  // SKITY_GRAPHIC_PAINT_HPP
