0da5eac9710452611bfe0129f2a4e60dd9159904
RuiwenTang <tangruiwen1989@gmail.com>
2021-04-19 22:00:48 +0800

    append quad_construct struct

---
+++ src/geometry/quad_construct.hpp
@@ -0,0 +1,17 @@
+
+#ifndef SKITY_SRC_GEOMETRY_QUAD_CONSTRUCT_HPP
+#define SKITY_SRC_GEOMETRY_QUAD_CONSTRUCT_HPP
+
+namespace skity {
+
+/**
+ * @sruct QuadConstruct
+ * The state of the quad stroke under construction
+ */
+struct QuadConstruct {
+};
+
+}  // namespace skity
+
+#endif  // SKITY_SRC_GEOMETRY_QUAD_CONSTRUCT_HPP
+
