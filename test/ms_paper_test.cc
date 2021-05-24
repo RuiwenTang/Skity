4264ab97b4e72ae33df7cf1a811aae18d25f84f8
RuiwenTang <tangruiwen1989@gmail.com>
2021-05-24 16:57:15 +0800

    split cubic demo

--- test/ms_paper_test.cc
+++ test/ms_paper_test.cc
@@ -3,10 +3,12 @@
 #include <vector>
 
 #include "common/test_common.hpp"
+#include "src/geometry/geometry.hpp"
 
 struct Mesh {
   GLuint vao = 0;
   GLuint vbo = 0;
+  GLuint count = 0;
 };
 
 class PaperTest : public test::TestApp {
@@ -44,6 +46,31 @@ class PaperTest : public test::TestApp {
 
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     glBindVertexArray(0);
+
+    glm::mat4 m = glm::translate(glm::identity<glm::mat4>(), {300, 0, 0});
+    glm::mat4 mm = mvp_ * m;
+
+    glUniformMatrix4fv(mvp_location_, 1, GL_FALSE, &mm[0][0]);
+
+    glBindVertexArray(mesh_cubic_.vao);
+    glBindBuffer(GL_ARRAY_BUFFER, mesh_cubic_.vbo);
+
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float),
+                          (void*)0);
+
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float),
+                          (void*)(2 * sizeof(float)));
+
+    glDrawArrays(GL_TRIANGLES, 0, mesh_cubic_.count);
+
+    glDisableVertexAttribArray(0);
+    glDisableVertexAttribArray(1);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+
     glUseProgram(0);
   }
 
@@ -59,11 +86,56 @@ class PaperTest : public test::TestApp {
 
     glGenVertexArrays(1, &mesh_.vao);
     glGenBuffers(1, &mesh_.vbo);
+    mesh_.count = 3;
 
     glBindVertexArray(mesh_.vao);
     glBindBuffer(GL_ARRAY_BUFFER, mesh_.vbo);
     glBufferData(GL_ARRAY_BUFFER, point_data.size() * sizeof(float),
                  point_data.data(), GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+
+    std::vector<skity::Point> cubics = {
+        skity::Point{10.f, 10.f, 0.f, 1.f},
+        skity::Point{10.f, 200.f, 0.f, 1.f},
+        skity::Point{200.f, 50.f, 0.f, 1.f},
+        skity::Point{200.f, 200.f, 0.f, 1.f},
+    };
+
+    std::array<skity::Point, 32> sub_cubics;
+    skity::SubDividedCubic8(cubics.data(), sub_cubics.data());
+    skity::Point start = cubics.front();
+    skity::Point center = sub_cubics[sub_cubics.size() / 2];
+    skity::Point end = cubics.back();
+    std::vector<float> cubic_vertices;
+    for (uint32_t i = 0; i < 8; i++) {
+      std::array<skity::Point, 3> quad;
+      skity::CubicToQuadratic(sub_cubics.data() + i * 4, quad.data());
+      cubic_vertices.emplace_back(quad[0].x);
+      cubic_vertices.emplace_back(quad[0].y);
+      cubic_vertices.emplace_back(0.f);
+      cubic_vertices.emplace_back(0.f);
+
+      cubic_vertices.emplace_back(quad[1].x);
+      cubic_vertices.emplace_back(quad[1].y);
+      cubic_vertices.emplace_back(0.f);
+      cubic_vertices.emplace_back(0.5f);
+
+      cubic_vertices.emplace_back(quad[2].x);
+      cubic_vertices.emplace_back(quad[2].y);
+      cubic_vertices.emplace_back(1.f);
+      cubic_vertices.emplace_back(1.f);
+    }
+
+    glGenVertexArrays(1, &mesh_cubic_.vao);
+    glGenBuffers(1, &mesh_cubic_.vbo);
+    mesh_cubic_.count = cubic_vertices.size() / 4;
+
+    glBindVertexArray(mesh_cubic_.vao);
+    glBindBuffer(GL_ARRAY_BUFFER, mesh_cubic_.vbo);
+    glBufferData(GL_ARRAY_BUFFER, cubic_vertices.size() * sizeof(float),
+                 cubic_vertices.data(), GL_STATIC_DRAW);
 
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     glBindVertexArray(0);
@@ -105,6 +177,7 @@ class PaperTest : public test::TestApp {
  private:
   glm::mat4 mvp_ = {};
   Mesh mesh_ = {};
+  Mesh mesh_cubic_ = {};
   GLuint program_ = 0;
   GLint mvp_location_ = -1;
 };
