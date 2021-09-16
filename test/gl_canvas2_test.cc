0b7e2f8aec3f22b197d50e577b74bbd98833f449
RuiwenTang <tangruiwen1989@gmail.com>
2021-09-16 17:55:52 +0800

    add new test case to test GLCanvas2

---
+++ test/gl_canvas2_test.cc
@@ -0,0 +1,55 @@
+
+#include <skity/graphic/paint.hpp>
+#include <skity/graphic/path.hpp>
+#include <skity/render/canvas.hpp>
+
+#include "test/common/test_common.hpp"
+
+class GLCanvas2Test : public test::TestApp {
+ public:
+  GLCanvas2Test() : test::TestApp() {}
+  ~GLCanvas2Test() override = default;
+
+ protected:
+  void OnInit() override {
+    canvas_ =
+        skity::Canvas::MakeGLCanvas2(0, 0, 800, 600, (void*)glfwGetProcAddress);
+
+    glClearColor(0.3, 0.4, 0.5, 1.0);
+    glClearStencil(0x00);
+    glStencilMask(0xff);
+
+    // Blend is need for anti-alias
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_ONE, GL_ONE_MINUS_SRC_ALPHA);
+  }
+  void OnDraw() override {
+    glClear(GL_COLOR_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
+
+    skity::Paint paint;
+    paint.setStyle(skity::Paint::kStroke_Style);
+    paint.setStrokeWidth(10.f);
+    paint.setStrokeCap(skity::Paint::kSquare_Cap);
+    paint.setStrokeJoin(skity::Paint::kMiter_Join);
+    paint.setColor(skity::Color_WHITE);
+
+    skity::Path path1;
+
+    path1.moveTo(100, 100);
+    path1.lineTo(300, 120);
+    path1.lineTo(400, 300);
+
+    canvas_->drawPath(path1, paint);
+
+    canvas_->flush();
+  }
+
+ private:
+  std::unique_ptr<skity::Canvas> canvas_;
+};
+
+int main(int argc, const char** argv) {
+  GLCanvas2Test app;
+  app.Start();
+  return 0;
+}
