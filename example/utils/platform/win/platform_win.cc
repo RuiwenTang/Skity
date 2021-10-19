22c94410cf4c590610b22d7726d70834a534f58c
RuiwenTang <tangruiwen1989@gmail.com>
2021-10-19 09:16:43 +0800

    Windows platform support create Vulkan surface

--- example/utils/platform/win/platform_win.cc
+++ example/utils/platform/win/platform_win.cc
@@ -5,6 +5,9 @@
 #include <GLFW/glfw3.h>
 #include <GLFW/glfw3native.h>
 
+#define VK_USE_PLATFORM_WIN32_KHR
+#include <vulkan/vulkan.hpp>
+
 namespace example {
 
 class PlatformWIN : public PlatformGLFW {
@@ -18,6 +21,29 @@ class PlatformWIN : public PlatformGLFW {
     }
 
     return glfwGetWin32Window(reinterpret_cast<GLFWwindow*>(window));
+  }
+
+  void* CreateSurface(void* instance, void* window) override {
+    vk::Win32SurfaceCreateInfoKHR create_info{
+        {}, nullptr, reinterpret_cast<HWND>(GetWindowSurface(window))};
+
+    vk::Instance vk_instance{static_cast<VkInstance>(instance)};
+
+    auto create_ret = vk_instance.createWin32SurfaceKHR(create_info);
+
+    if (create_ret.result != vk::Result::eSuccess) {
+      return nullptr;
+    } else {
+      return create_ret.value;
+    }
+  }
+
+  std::vector<const char*> GetRequiredInstanceExtensions() override {
+    auto extensions = PlatformGLFW::GetRequiredInstanceExtensions();
+
+    extensions.emplace_back("VK_KHR_win32_surface");
+
+    return extensions;
   }
 };
 
