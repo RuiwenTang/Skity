b2723b31254ab9200601e4ce12e1d4805fb48a3d
RuiwenTang <tangruiwen1989@gmail.com>
2022-08-03 15:17:00 +0800

    Fix libpng compile error

--- cmake/Platform.cmake
+++ cmake/Platform.cmake
@@ -45,17 +45,6 @@ if (EMSCRIPTEN)
 
   target_compile_options(skity PUBLIC "-s" "USE_FREETYPE=1")
   set_target_properties(skity PROPERTIES LINK_FLAGS "-s USE_FREETYPE=1")
-elseif(ANDROID)
-  # TODO support android build
-  message("build for Android")
-  add_definitions(-DSKITY_ANDROID=1)
-  # Freetype is find outside
-  if(${FREETYPE_FOUND})
-    message("freetype found for android")
-    add_definitions(-DENABLE_TEXT_RENDER=1)
-    target_include_directories(skity PRIVATE ${FREETYPE_INCLUDE})
-    target_link_libraries(skity PRIVATE freetype)
-  endif()
 else()
   # Fixme to solve path issue
   include(cmake/CommonDep.cmake)
