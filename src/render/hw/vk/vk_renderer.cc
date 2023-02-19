37de40a0fc4539488a47873a358f0ba9dd629eef
RuiwenTang <tangruiwen1989@gmail.com>
2023-02-19 16:29:13 +0800

    vulkan RenderPass optimize

--- src/render/hw/vk/vk_renderer.cc
+++ src/render/hw/vk/vk_renderer.cc
@@ -518,7 +518,7 @@ void VkRenderer::InitOffScreenRenderPass() {
   attachments[1].loadOp = VK_ATTACHMENT_LOAD_OP_CLEAR;
   attachments[1].storeOp = VK_ATTACHMENT_STORE_OP_STORE;
   attachments[1].stencilLoadOp = VK_ATTACHMENT_LOAD_OP_CLEAR;
-  attachments[1].stencilStoreOp = VK_ATTACHMENT_STORE_OP_STORE;
+  attachments[1].stencilStoreOp = VK_ATTACHMENT_STORE_OP_DONT_CARE;
   attachments[1].initialLayout = VK_IMAGE_LAYOUT_UNDEFINED;
   attachments[1].finalLayout = VK_IMAGE_LAYOUT_DEPTH_STENCIL_ATTACHMENT_OPTIMAL;
 
