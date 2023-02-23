0200cfa5576e8f077f4c831666fd0dfebffc2d30
RuiwenTang <tangruiwen1989@gmail.com>
2023-02-23 23:18:38 +0800

    Fix compile error when using vcpkg (#40)

--- cmake/CommonDep.cmake
+++ cmake/CommonDep.cmake
@@ -1,6 +1,11 @@
 
-add_subdirectory(third_party/freetype)
+if (SKITY_VCPKG)
+    find_package(freetype CONFIG REQUIRED)
+else()
+    add_subdirectory(third_party/freetype)
 
-add_definitions(-DENABLE_TEXT_RENDER=1)
+    add_definitions(-DENABLE_TEXT_RENDER=1)
 
+endif()
+    
 target_link_libraries(skity PRIVATE freetype)
